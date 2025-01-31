64d5b1b33aca50a9c5eb9201b8fc863c8905535b
RobyLedda94 <robyledda01@yahoo.it>
2025-01-31 12:27:13 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Dato un array, mostra gli elementi a video a seguito del click di un bottone attraverso l'utilizzo del ciclo While
+
+
+// Dichiarazione dell'array 
+let lista_spesa = ['Pasta', 'Pomodori', 'Melanzane', 'Salumi', 'Frutta', 'Bevande'];
+
+// Recupero gli elementi dal DOM
+
+
+// recupero il bottone 
+let btn_lista_spesa = document.getElementById('btn-lista-spesa');
+
+// recupero l'elemento che mostra il risultato a video
+
+let ul_lista_spesa = document.getElementById('lista-spesa');
---
+++ readme.md
@@ -0,0 +1,3 @@
+Esercizi JS, argomenti Variabili, Istruzioni Condizionali e Switch , Ciclo For - While - Do While, Array
+
+
